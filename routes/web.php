cad3f7be9fb5b9de3880ded4b5899e0f4e61e9fb
Salsa019 <asaauliaah@gmail.com>
2026-05-20 11:44:45 +0700

    pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PegawaiDBController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,6 +21,8 @@ Route::get('blog', function () {
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+// kumpulan tugas
 Route::get('menu', [PageController::class, 'idx']);
 Route::get('intro', [PageController::class, 'p2intro']);
 Route::get('news', [PageController::class, 'p2news']);
@@ -34,10 +37,15 @@ Route::get('linktree', [PageController::class, 'p5linktree']);
 Route::get('pra-ets', [PageController::class, 'tugas_ets']);
 Route::get('ets', [PageController::class, 'ets_asa']);
 
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+// pertemuan 9
+Route::get('/pegawailama/{nama}', [PegawaiController::class, 'index']);
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
-//blog
+
+// blog
 Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+//route CRUD
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
