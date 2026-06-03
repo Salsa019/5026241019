43b06a5c3520fa40728faece7b1f13adbb028ba7
Salsa019 <asaauliaah@gmail.com>
2026-06-03 12:29:03 +0700

    pertemuan 13

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,20 @@
-<!DOCTYPE html>
-<html>
+@extends('template')
+@section('judul_halaman', 'Data Pegawai')
+@section('konten')
 
-<head>
-    <title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
+    <p>
+        <br><a href="/pegawai/tambah" class="btn btn-primary">Tambah Pegawai Baru</a>
+    </p>
 
-<body>
-
-    <h2>www.malasngoding.com</h2>
-    <h3>Data Pegawai</h3>
-
-    <a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
+    <p>Cari Data Pegawai :</p>
+    <form action="/pegawai/cari" method="GET">
+        <input type="text" name="cari" placeholder="Cari Pegawai .." class="form-control">
+        <input type="submit" value="CARI" class="btn btn-success">
+    </form>
 
     <br />
-    <br />
 
-    <table border="1">
+    <table class="table table-striped table-hover">
         <tr>
             <th>Nama</th>
             <th>Jabatan</th>
@@ -30,18 +29,12 @@
                 <td>{{ $p->pegawai_umur }}</td>
                 <td>{{ $p->pegawai_alamat }}</td>
                 <td>
-                    <a href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
+                    <a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
                     |
-                    <a href="/pegawai/hapus/{{ $p->pegawai_id }}">Hapus</a>
+                    <a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
                 </td>
             </tr>
         @endforeach
     </table>
-
-    <?php
-    echo $pegawai;
-    ?>
-
-</body>
-
-</html>
+    {{ $pegawai->links() }}
+@endsection
